e1763558f91fe6deb32a5473fd52adcc09363875
Joshua Eze <ezejoshuac@gmail.com>
2023-07-02 13:50:53 +0100

    Extend the DbContext for the application database
    - Configure the connnection
    - Register the dbcontext service

---
+++ Url-Shortener.API/Url-Shortener.Data/Context/ApplicationDbContext.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace Url_Shortener.Data.Context
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options){ }
+    }
+}
