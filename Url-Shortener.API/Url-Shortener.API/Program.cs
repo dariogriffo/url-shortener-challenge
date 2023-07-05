9d831833238375b710af80e5ddb6c4345a29cb1c
Joshua Eze <ezejoshuac@gmail.com>
2023-07-05 22:44:47 +0100

    Register services
    - Register db connection extension
    - Register bll services extension

--- Url-Shortener.API/Url-Shortener.API/Program.cs
+++ Url-Shortener.API/Url-Shortener.API/Program.cs
@@ -1,8 +1,12 @@
+using Url_Shortener.API.Extensions;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
 builder.Services.AddControllers();
+builder.Services.AddDbConnection(builder.Configuration);
+builder.Services.RegisterServices();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
